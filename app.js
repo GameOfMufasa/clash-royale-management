f7d41197c728803a2809e3f3a2913068152bcb00
Shai Cohen <shai@similartech.com>
2018-09-04 13:07:00 +0300

    everything is working

---
+++ app.js
@@ -0,0 +1,112 @@
+const request = require("request");
+const util = require('util');
+const Promise = require("promise");
+const moment = require("moment");
+const fs = require('fs');
+const config = require("config");
+
+const baseUrl = "https://api.clashroyale.com/v1/clans/%s/";
+
+const apiInfo = {
+    clanInfo: baseUrl + "",
+    clanMembers: baseUrl + "members",
+    clanWarLog: baseUrl + "warlog",
+    authKey: "Bearer " + config.get("authKey")
+};
+
+const makeGetRequest = (url) => {
+    console.log(url);
+    return new Promise((resolve, reject) => {
+        request({
+            headers: {
+                "Accept": "application/json",
+                "authorization": apiInfo.authKey
+            },
+            uri: url,
+            method: 'GET'
+        }, function (err, res, body) {
+            let json = JSON.parse(res.body);
+            if(json.reason) {
+                reject(json);
+            } else {
+                resolve(json);
+            }
+        });
+    });
+};
+
+const getClanMembers = clanID => makeGetRequest(util.format(apiInfo.clanMembers, clanID));
+
+const getWarLog = (clanID, limit) => makeGetRequest(util.format(apiInfo.clanWarLog + "?limit=" + limit, clanID));
+
+module.exports = (async () => {
+    try {
+        const _ = require("underscore");
+
+        const defaults = config.get("defaults");
+        const clanIDFull = config.get("clan").id.replace("#", "%23");
+        const full_data_file = config.get("server").files.full_data;
+        const data_file = config.get("server").files.datatable_json;
+        const date_format = config.get("server").date_format;
+        const hasOlderData = fs.existsSync(full_data_file);
+        const nowTimeForFirstSeen = moment();
+
+        let oldRecordsUsers = {};
+
+        if (hasOlderData) {
+            let oldRecordsUsersAsync = new Promise((resolve, reject) => {
+                fs.readFile(full_data_file, 'utf8', (err, data) => {
+                    resolve(JSON.parse(data));
+                });
+            });
+
+            oldRecordsUsers = await oldRecordsUsersAsync;
+            oldRecordsUsers = _.mapObject(oldRecordsUsers, val => {
+                val = Object.assign(_.extend({}, defaults), val);
+                val.firstSeen = val.firstSeen || nowTimeForFirstSeen.format(date_format);
+                return val;
+            });
+        }
+
+        let members = await getClanMembers(clanIDFull);
+        let currentUsers = {};
+
+        for (let user of members.items) {
+            if(!oldRecordsUsers[user.tag]) {
+                currentUsers[user.tag] = _.extend({}, defaults);
+                currentUsers[user.tag].firstSeen = nowTimeForFirstSeen.format(date_format);
+            } else {
+                currentUsers[user.tag] = oldRecordsUsers[user.tag];
+            }
+            currentUsers[user.tag].tag = user.tag;
+            currentUsers[user.tag].name = user.name;
+            currentUsers[user.tag].role = user.role;
+            currentUsers[user.tag].trophies = user.trophies;
+        }
+
+        let clanWars = await getWarLog(clanIDFull, config.get("clan").past_war_logs_limit);
+
+        for (let warLog of clanWars.items) {
+            let warDate = moment(warLog.createdDate, "YYYYMMDDTHH:mm:ss.SSSZ");
+            for (let participant of warLog.participants) {
+                if (currentUsers[participant.tag]) {
+                    if (warDate.valueOf() > (currentUsers[participant.tag].lastPlayed || 0)) {
+                        currentUsers[participant.tag].battlesPlayed = (currentUsers[participant.tag].battlesPlayed || 0) + 1;
+                        currentUsers[participant.tag].lastPlayed = warDate.valueOf();
+                        currentUsers[participant.tag].lastPlayedString = warDate.format(date_format);
+
+                    }
+                }
+            }
+        }
+
+        fs.writeFile(full_data_file, JSON.stringify(currentUsers, null, 2), 'utf8', () => {
+        });
+
+        let array = Object.values(currentUsers);
+        fs.writeFile(data_file, JSON.stringify({"data": array}, "", 2), 'utf8', () => {
+        });
+    } catch (ex) {
+        return ex;
+    }
+});
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fs = require('fs');
+const config = require("config");
+const port = config.get("server").port;
+const app = express();
+
+const hostname = config.get("server").hostname;
+
+(async () => {
+    if (!fs.existsSync(config.get("server").files.datatable_json)) {
+        await require('./app')();
+    }
+
+    app.get('/', (req, res) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(fs.readFileSync(__dirname + '/html/index.html'));
+    });
+
+    app.get('/data', (req, res) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(fs.readFileSync(__dirname + '/'+config.get("server").files.datatable_json));
+    });
+
+    app.get('/refresh', async (req, res) => {
+        let response = await require('./app')();
+        if(response && response.reason) {
+            res.status(401).json(response);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+
+    app.listen(port, () => {
+        console.log(`
+        Server is running at http://${hostname}:${port}/ 
+        Server hostname ${hostname} is listening on port ${port}!
+    `);
+    });
+})();
